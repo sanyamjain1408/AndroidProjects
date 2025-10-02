3adde5b5c4a913741d06ff4c6a5be414ddb226dc
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-10-03 00:04:56 +0530

    Create new small projects

---
+++ 20_get_data_using_volley/app/src/main/java/com/example/a20_get_data_using_volley/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.a20_get_data_using_volley
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a20_get_data_using_volley.ui.theme._20_get_data_using_volleyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _20_get_data_using_volleyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _20_get_data_using_volleyTheme {
+        Greeting("Android")
+    }
+}
---
+++ 20_get_data_using_volley/app/src/main/java/com/example/a20_get_data_using_volley/userinformation.kt
@@ -0,0 +1,3 @@
+package com.example.a20_get_data_using_volley
+
+class userinformation : ArrayList<userinformationItem>()
